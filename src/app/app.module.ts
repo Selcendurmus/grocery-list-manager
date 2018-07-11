e7721da7f2871d42d690020b214991cfecdc08f9
Selcendurmus <40351210+Selcendurmus@users.noreply.github.com>
2018-07-11 12:43:03 -0700

    adding my-grocery-list component and bootstrap library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, APP_BOOTSTRAP_LISTENER } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyGroceryListComponent } from './my-grocery-list/my-grocery-list.component';
+import {FormsModule} from '@angular/Forms';
+import {HttpModule} from '@angular/http'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyGroceryListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    
+   
+
+   
   ],
   providers: [],
-  bootstrap: [AppComponent]
+    bootstrap: [MyGroceryListComponent]
 })
 export class AppModule { }
---
+++ src/app/my-grocery-list/my-grocery-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-grocery-list',
+  templateUrl: './my-grocery-list.component.html',
+  styleUrls: ['./my-grocery-list.component.css']
+})
+export class MyGroceryListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
